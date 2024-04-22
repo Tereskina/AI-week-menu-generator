0756e412f11caa7078bb3521c8b9dfa2365bbf19
Kseniia Tereskina <39565570+Tereskina@users.noreply.github.com>
2024-04-22 14:29:16 +0100

    Added AI generator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
+function displayMenu(response) {
+  console.log("menu generated");
+  new Typewriter("#menu", {
+    // strings: "Please wait. Your menu is being generated...",
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 0.1,
+  });
+}
+
 function generateMenu(event) {
   event.preventDefault();
   let menuElement = document.getElementById("menu");
+  menuElement.innerHTML = "Please Wait. You recepies is being generated... ";
+  let userInstructions = document.getElementById("user-instructions");
 
-  new Typewriter("#menu", {
-    strings: "Please wait. You menu is being generated...",
-    autoStart: true,
-    cursor: "",
-    delay: 1,
-  });
+  let apiKey = "af452f84910t3od515bb3246f723ee9b";
+  let context =
+    "You are experienced cook. Provide answer in HTML. Start each of 2 recepies in a new paragraph, provide ingreedients and cooking instructions. Use <strong> for the title of the recipies.  Separate recepies with <hr> ";
+  let prompt = `Write menu for 2 dinners with recipies using ingredients from ${userInstructions.value} `;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(prompt);
+  axios.get(apiUrl).then(displayMenu);
 }
 
 let menuFormElement = document.getElementById("menu-form-element");
 menuFormElement.addEventListener("submit", generateMenu);
+Added
