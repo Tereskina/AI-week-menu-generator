4a631dac046d00b29865e8a61c43e2c1a6ad01b9
Kseniia Tereskina <39565570+Tereskina@users.noreply.github.com>
2024-04-22 13:07:28 +0100

    Basic JavaScript added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateMenu(event) {
+  event.preventDefault();
+  let menuElement = document.getElementById("menu");
 
+  new Typewriter("#menu", {
+    strings: "Please wait. You menu is being generated...",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let menuFormElement = document.getElementById("menu-form-element");
+menuFormElement.addEventListener("submit", generateMenu);
